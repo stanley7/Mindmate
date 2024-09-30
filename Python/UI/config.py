6449bb2c8e8053ed1bc2c2c2fabb749c8f2e4923
stanley7 <stanleyjoel25@gmail.com>
2024-09-30 23:28:02 +0200

    Create config.py

---
+++ Python/UI/config.py
@@ -0,0 +1,18 @@
+import os
+from dotenv import load_dotenv
+
+# Load environment variables
+load_dotenv()
+
+# Sender email credentials
+SENDER_EMAIL = "warp2899@gmail.com"
+SENDER_PASSWORD = "peju voio adnu kbhn"  # Replace with your app password or correct credentials
+
+# Room names and corresponding moderator emails
+ROOMS = {
+    "MindMate Room 1": "quadrasmack@gmail.com",
+    "MindMate Room 2": "quadrasmack@gmail.com",
+    "MindMate Room 3": "quadrasmack@gmail.com",
+    "MindMate Room 4": "quadrasmack@gmail.com",
+    "MindMate Room 5": "quadrasmack@gmail.com"
+}
